f62564ddf2f9958b20e4be149817b0a27fc11b75
laohu <laohubbb@gmail.com>
2022-09-09 15:49:27 +0800

    制作switch组件

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,7 @@ import {createWebHistory,createRouter} from 'vue-router'
 import Home from './views/Home.vue';
 import Doc from './views/Doc.vue';
 import DocDemo from './components/DocDemo.vue';
-import Switch from './components/Switch.vue';
+import Switch from './components/SwitchDemo.vue';
 import Dialog from './components/Dialog.vue';
 import Tabs from './components/Tabs.vue';
 import button from './components/Button.vue';
