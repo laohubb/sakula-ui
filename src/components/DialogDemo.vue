bfb2f7bdfe0836b3fdc504a3c14e3f2c6bd91836
laohu <laohubbb@gmail.com>
2022-09-10 17:53:21 +0800

    用具名插槽实现dialog自定义标题内容

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,7 +2,16 @@
   <div>Dialog 示例</div>
   <h1>示例1</h1>
   <Button @click="toggle">示例</Button>
-  <Dialog v-model:visible="x" :closeOnClickOverlay="false" :ok="fn1" :cancel="fn2"></Dialog>
+  <Dialog v-model:visible="x" :closeOnClickOverlay="false"
+          :ok="fn1" :cancel="fn2">
+    <template v-slot:content>
+      <strong>hi</strong>
+      <div>hi2</div>
+    </template>
+  <template v-slot:title>
+    <strong>加粗的标题</strong>
+  </template>
+  </Dialog>
 </template>
 
 <script lang="ts">
