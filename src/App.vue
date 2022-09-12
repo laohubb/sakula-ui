0edb9634ef83df4cec316c48d86f74e7c755395b
laohu <laohubbb@gmail.com>
2022-09-12 20:03:14 +0800

    禁用移动端双击缩放

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,45 @@
 </template>
 
 <script>
-import {provide,ref} from 'vue'
+import {onMounted, provide, ref} from 'vue'
 import {router} from "./router";
 
 export default {
   name: 'App',
-  setup(){
-    const width=document.documentElement.clientWidth;
-    const menuVisible=ref(width<=300?false:true )
-    provide("menuVisible",menuVisible)
-    router.afterEach(()=>{
-      if(width<=500){
-        menuVisible.value=false
+  setup() {
+    const width = document.documentElement.clientWidth;
+    const menuVisible = ref(width <= 300 ? false : true)
+    provide("menuVisible", menuVisible)
+    router.afterEach(() => {
+      if (width <= 500) {
+        menuVisible.value = false
       }
     })
+
+    onMounted(()=>{
+
+      // 禁止页面缩放
+      window.onload = function () {
+        document.addEventListener("touchstart", function (event) {
+          if (event.touches.length > 1) {
+            event.preventDefault();
+          }
+        });
+        document.addEventListener("gesturestart", function (event) {
+          event.preventDefault();
+        });
+      };
+      document.documentElement.addEventListener(
+          "touchmove",
+          function (event) {
+            if (event.touches.length > 1) {
+              event.preventDefault();
+            }
+          },
+          false
+      );
+    })
+
   }
 }
 </script>
